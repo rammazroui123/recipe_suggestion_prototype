e95258fa3ff2727f712a2d52064f6327693f444b
Deloris <dxn291@student.bham.ac.uk>
2024-12-02 15:39:57 +0000

    I added Users table creation to database schema

--- database/sql_files.sql
+++ database/sql_files.sql
@@ -1,2 +1,10 @@
 --Create DATABASE
 CREATE DATABASE recipe_suggestion 
+--create users table
+CREATE TABLE users (
+	user_id SERIAL PRIMARY KEY,
+	username VARCHAR(50) NOT NULL,
+	email VARCHAR(100) NOT NULL,
+	dietery_preferences TEXT,
+	profile_picture TEXT
+);
