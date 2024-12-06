97b45a5f7de3d6f66820ae88394dc9efc60bb41e
Deloris <dxn291@student.bham.ac.uk>
2024-12-06 13:32:36 +0000

    Updated .gitignore and added sql_files.sql

--- database/update_file.sql
+++
@@ -1,9 +0,0 @@
---Altering the Users table
--- Step 1: Add the password column to the Users table
-ALTER TABLE Users
-ADD COLUMN password VARCHAR(100);
-
--- Step 2: Update the password to 'default_password' where it is NULL
-UPDATE Users
-SET password = 'default_password'
-WHERE password IS NULL;
