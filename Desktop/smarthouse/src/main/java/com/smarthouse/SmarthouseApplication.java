14bbd516808c42ae2ddc129bced8f108cb27ee6a
SukhnaazRandhawa <sukhnaazrandhawa100@gmail.com>
2024-12-06 15:39:57 +0000

    Removed nested Git repository reference

---
+++ Desktop/smarthouse/src/main/java/com/smarthouse/SmarthouseApplication.java
@@ -0,0 +1,13 @@
+package com.smarthouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmarthouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmarthouseApplication.class, args);
+	}
+
+}
---
+++ Desktop/smarthouse/src/test/java/com/smarthouse/SmarthouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarthouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmarthouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
